86702c4ac9ffcdc7d26fe461434617ca11d0bbd7
markbotsjc <97488742+markbotsjc@users.noreply.github.com>
2022-01-10 22:35:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const app = express();
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Bem vindo humano');
+});
+
+app.listen(3000, () => console.log('connected the the services, please check the above for the following steps'));
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+//when its ready log it
+client.on("ready", ()=>console.log("READY"));
+//define welcome "package"
+const welcome = require("./welcome");
+welcome(client);
+//start the bot
+client.login(config.TOKEN);
+
+
+client.on("ready", () => {
+    console.log(`${client.user.tag} Está pronto.`)
+    const server = client.voice.connections.size
+    client.user.setActivity({ type: "WATCHING", name: `Humanos Entrar no Clã, Bem vindo Humano!` })
+})
+//NOTE:
+/*
+THis is the config.json File
+
+"TOKEN"           ... is your Bot token
+"CHANNEL_WELCOME" ... is the Channel ID of your welcome channel
+"ROLES_WELCOME"   ... are all of the Role IDs you wanna add to the user when he joins the server, it must be an array and can be unlimited!
+
+{
+  "TOKEN":  "",
+  "CHANNEL_WELCOME": "",
+  "ROLES_WELCOME": ["",""]
+}
+
+*/
---
+++ welcome.js
@@ -0,0 +1,90 @@
+const config = require("./config");
+const Canvas = require("canvas");
+const Discord = require("discord.js");
+const { registerFont, createCanvas } = require('canvas')
+registerFont('./Roboto-Regular.ttf', { family: 'Roboto' })
+
+module.exports = function (client) {
+
+    const description = {
+        name: "WelcomeImages",
+        filename: "welcome.js",
+        version: "4.8"
+    }
+    //log that the module is loaded
+    console.log(` :: ⬜️ Module: ${description.name} | Loaded version ${description.version} from ("${description.filename}")`)
+    //fires every time when someone joins the server
+    client.on("guildMemberAdd", async member => {
+      //If not in a guild return
+      if(!member.guild) return;
+      //create a new Canvas
+      const canvas = Canvas.createCanvas(1772, 633);
+      //make it "2D"
+      const ctx = canvas.getContext('2d');
+      //set the Background to the welcome.png
+      const background = await Canvas.loadImage(`./welcome.png`);
+      ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+      ctx.strokeStyle = '#f2f2f2';
+      ctx.strokeRect(0, 0, canvas.width, canvas.height);
+      //set the first text string 
+      var textString3 = `${member.user.username}`;
+      //if the text is too big then smaller the text
+      if (textString3.length >= 14) {
+        ctx.font = 'bold 100px "Roboto"';
+        ctx.fillStyle = '#f2f2f2';
+        ctx.fillText(textString3, 800, canvas.height / 2 + 1);
+      }
+      //else dont do it
+      else {
+        ctx.font = 'bold 150px "Roboto"';
+        ctx.fillStyle = '#f2f2f2';
+        ctx.fillText(textString3, 720, canvas.height / 2 + 20);
+      }
+      //define the Discriminator Tag
+      var textString2 = `#${member.user.discriminator}`;
+
+      ctx.font = 'bold 40px "Roboto"';
+      ctx.fillStyle = '#f2f2f2';
+      ctx.fillText(textString2, 1640, canvas.height / 2 + 58);
+      //define the Member count
+      var textString4 = `Se tornou um Piveti Agora` ;
+      ctx.font = 'bold 60px "Roboto"';
+      ctx.fillStyle = '#f2f2f2';
+      ctx.fillText(textString4, 950, canvas.height / 2 + 125);
+      //get the Guild Name
+      var textString4 = `${member.guild.name}`;
+      ctx.font = 'bold 60px "Roboto"';
+      ctx.fillStyle = '#f2f2f2';
+      ctx.fillText(textString4, 1050, canvas.height / 2 - 150);
+      //create a circular "mask"
+      ctx.beginPath();
+      ctx.arc(315, canvas.height / 2, 250, 0, Math.PI * 2, true);//position of img
+      ctx.closePath();
+      ctx.clip();
+      //define the user avatar
+      const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+      //draw the avatar
+      ctx.drawImage(avatar, 65, canvas.height / 2 - 250, 500, 500);
+      //get it as a discord attachment
+      const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+      //define the welcome embed
+      const welcomeembed = new Discord.MessageEmbed()
+        .setColor("RANDOM")
+        .setTimestamp()
+        .setFooter("Entrou", member.guild.iconURL({ dynamic: true }))
+        .setDescription(`**Bem Vindo(a) a ${member.guild.name}, Humano.**
+      Ei <@${member.id}>! Leia as Regras! `)
+        .setImage("attachment://welcome-image.png")
+        .attachFiles(attachment);
+      //define the welcome channel
+      const channel = member.guild.channels.cache.find(ch => ch.id === config.CHANNEL_WELCOME);
+      //send the welcome embed to there
+      channel.send(welcomeembed);
+      //member roles add on welcome every single role
+      let roles = config.ROLES_WELCOME;
+      for(let i = 0; i < roles.length; i++ )
+      member.roles.add(roles[i]);
+    })
+}
+
+//Coded by Navaneeth K M!
